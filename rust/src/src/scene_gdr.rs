972c6346e13ed17f6b3f5175d9a4c186adcfa7da
ABC3Dz <archueyouler@gmail.com>
2026-05-16 05:26:07 +0700

    Delete rust/src directory

--- rust/src/src/scene_gdr.rs
+++
@@ -1,8 +0,0 @@
-use godot::prelude::*;
-use godot::classes::{CharacterBody3D, ICharacterBody3D};
-
-#[derive(GodotClass)]
-#[class(init, base=CharacterBody3D)]
-pub struct SceneGDR {
-    base: Base<CharacterBody3D>,
-}
