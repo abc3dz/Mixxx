b2a85243be343a489d2a5572d55be5fbdedd22de
ABC3Dz <archueyouler@gmail.com>
2026-05-22 11:31:54 +0700

    Delete rust directory

--- rust/scene_gdr.rs
+++
@@ -1,8 +0,0 @@
-use godot::prelude::*;
-use godot::classes::{CharacterBody3D};
-
-#[derive(GodotClass)]
-#[class(init, base=CharacterBody3D)]
-pub struct SceneGDR {
-    base: Base<CharacterBody3D>,
-}
--- rust/scene_gdt.rs
+++
@@ -1,8 +0,0 @@
-use godot::prelude::*;
-use godot::classes::{CharacterBody3D};
-
-#[derive(GodotClass)]
-#[class(init, base=CharacterBody3D)]
-pub struct SceneGDT {
-    base: Base<CharacterBody3D>,
-}
